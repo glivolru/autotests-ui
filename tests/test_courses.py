4a9d5f9f9e788bd4aba2ff8d7b414bdc50fe57d0
glivolru <ivanglushenkov@yandex.ru>
2026-04-15 23:20:01 +0400

    fix test_empty_courses_list

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,21 +1,21 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect
 
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(chromium_page, chromium_page_with_state):
+def test_empty_courses_list(chromium_page_with_state):
 
-    chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-    courses_title = chromium_page.get_by_test_id("courses-list-toolbar-title-text")
+    courses_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
     expect(courses_title).to_be_visible()
 
-    folder_icon = chromium_page.get_by_test_id("courses-list-empty-view-icon")
+    folder_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
     expect(folder_icon).to_be_visible()
 
-    empty_result_text = chromium_page.get_by_test_id("courses-list-empty-view-title-text")
+    empty_result_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
     expect(empty_result_text).to_be_visible()
 
-    empty_result_description = chromium_page.get_by_test_id("courses-list-empty-view-description-text")
+    empty_result_description = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
     expect(empty_result_description).to_be_visible()
