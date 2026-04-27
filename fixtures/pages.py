6b87eff37ee6f90acc3e152922e926966e71e55a
glivolru <ivanglushenkov@yandex.ru>
2026-04-27 15:08:34 +0400

    Add 2 pages

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,8 +2,20 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.login_page import LoginPage
+from pages.dashboard_page import DashboardPage
+from pages.courses_page import CoursesPage
 
 
 @pytest.fixture
-def login_page(chromium_page:Page) -> LoginPage:
+def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
+
+
+@pytest.fixture
+def dashboard_page(chromium_page: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page)
+
+
+@pytest.fixture
+def courses_list_page(chromium_page: Page) -> CoursesPage:
+    return CoursesPage(page=chromium_page)
